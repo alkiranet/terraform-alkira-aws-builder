16e65b3103ed3b8a3293065be268068b9b6a744f
wcollins <william.levi.collins@gmail.com>
2023-12-24 20:28:25 -0500

    Remove old files

--- examples/connect-existing-vpcs/main.tf
+++
@@ -1,7 +0,0 @@
-module "aws_vpcs" {
-  source  = "alkiranet/aws-builder/alkira"
-
-  # Path to .yaml configuration files
-  config_file = "./aws_vpcs.yaml"
-
-}
--- examples/connect-existing-vpcs/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "connector_aws_id" {
-  value = try(module.aws_connectors.connector_aws_id, "")
-}
